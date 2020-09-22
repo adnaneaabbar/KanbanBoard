8b089ad8d315ddf906e8d5a4724d1a0b1280068b
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-09-22 22:04:09 +0100

    installing dependencies

--- frontend/kanbanboard/src/App.js
+++ frontend/kanbanboard/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <div className='App'>
-            <h1>Welcome to the KanbanBoard</h1>
+            <h1 className='alert alert-primary'>Welcome to the KanbanBoard</h1>
         </div>
     );
 }
