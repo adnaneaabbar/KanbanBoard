fe5505e933f9c6c4d60b27d9a774dc87578afca7
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-09-23 01:53:22 +0100

    routing and links

--- frontend/kanbanboard/src/App.js
+++ frontend/kanbanboard/src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Board from './components/Board';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
 
-function App() {
-    return (
-        <div className='App'>
-            <Navbar />
-            <Board />
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className='App'>
+                    <Navbar />
+                    <Route
+                        exact
+                        path='/addProjectTask'
+                        component={AddProjectTask}
+                    />
+                    <Route exact path='/' component={Board} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
