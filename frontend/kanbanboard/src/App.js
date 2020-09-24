c99c9f1792f0257e0e53bb60e9f0a67a8e0df51e
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-09-24 02:34:47 +0100

    starting on the redux store

--- frontend/kanbanboard/src/App.js
+++ frontend/kanbanboard/src/App.js
@@ -5,21 +5,25 @@ import Navbar from './components/Navbar';
 import Board from './components/Board';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AddProjectTask from './components/ProjectTask/AddProjectTask';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div className='App'>
-                    <Navbar />
-                    <Route
-                        exact
-                        path='/addProjectTask'
-                        component={AddProjectTask}
-                    />
-                    <Route exact path='/' component={Board} />
-                </div>
-            </Router>
+            <Provider store={store}>
+                <Router>
+                    <div className='App'>
+                        <Navbar />
+                        <Route
+                            exact
+                            path='/addProjectTask'
+                            component={AddProjectTask}
+                        />
+                        <Route exact path='/' component={Board} />
+                    </div>
+                </Router>
+            </Provider>
         );
     }
 }
