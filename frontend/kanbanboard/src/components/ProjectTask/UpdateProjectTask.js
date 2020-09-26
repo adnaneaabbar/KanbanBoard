cc81f52b330525912cb11d9ca8651bdad1b8f48c
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-09-26 04:57:41 +0100

    done

--- frontend/kanbanboard/src/components/ProjectTask/UpdateProjectTask.js
+++ frontend/kanbanboard/src/components/ProjectTask/UpdateProjectTask.js
@@ -2,16 +2,64 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getProjectTask } from '../../actions/projectTaskActions';
+import {
+    getProjectTask,
+    addProjectTask,
+} from '../../actions/projectTaskActions';
 import classnames from 'classnames';
 
 class UpdateProjectTask extends Component {
+    //intial state
+    constructor() {
+        super();
+        this.state = {
+            summary: '',
+            acceptanceCriteria: '',
+            status: '',
+            errors: {},
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+        const {
+            id,
+            summary,
+            acceptanceCriteria,
+            status,
+        } = nextProps.project_task;
+
+        this.setState({ id, summary, acceptanceCriteria, status });
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const updatedProjectTask = {
+            id: this.state.id,
+            summary: this.state.summary,
+            acceptanceCriteria: this.state.acceptanceCriteria,
+            status: this.state.status,
+        };
+        this.props.addProjectTask(updatedProjectTask, this.props.history);
+    }
+
     componentDidMount() {
         const pt_id = this.props.match.params.pt_id;
         this.props.getProjectTask(pt_id);
     }
 
     render() {
+        const err = this.state.errors;
         return (
             <div className='updateProjectTask'>
                 <div className='container'>
@@ -23,26 +71,42 @@ class UpdateProjectTask extends Component {
                             <h4 className='display-4 text-center'>
                                 Update a Project Task
                             </h4>
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className='form-group'>
                                     <input
                                         type='text'
-                                        className='form-control form-control-lg'
+                                        className={classnames(
+                                            'form-control form-control-lg',
+                                            {
+                                                'is-invalid': err.summary,
+                                            }
+                                        )}
                                         name='summary'
+                                        value={this.state.summary}
+                                        onChange={this.onChange}
                                         placeholder='Project Task summary'
                                     />
+                                    {err.summary && (
+                                        <div className='invalid-feedback'>
+                                            {err.summary}
+                                        </div>
+                                    )}
                                 </div>
                                 <div className='form-group'>
                                     <textarea
                                         className='form-control form-control-lg'
                                         placeholder='Acceptance Criteria'
                                         name='acceptanceCriteria'
+                                        value={this.state.acceptanceCriteria}
+                                        onChange={this.onChange}
                                     ></textarea>
                                 </div>
                                 <div className='form-group'>
                                     <select
                                         className='form-control form-control-lg'
                                         name='status'
+                                        value={this.state.status}
+                                        onChange={this.onChange}
                                     >
                                         <option value=''>Select Status</option>
                                         <option value='TO_DO'>TO DO</option>
@@ -67,13 +131,16 @@ class UpdateProjectTask extends Component {
 
 UpdateProjectTask.propTypes = {
     getProjectTask: PropTypes.func.isRequired,
+    addProjectTask: PropTypes.func.isRequired,
     project_task: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-    project_task: state.project_task,
+    project_task: state.project_tasks.project_task,
     errors: state.errors,
 });
 
-export default connect(mapStateToProps, { getProjectTask })(UpdateProjectTask);
+export default connect(mapStateToProps, { getProjectTask, addProjectTask })(
+    UpdateProjectTask
+);
